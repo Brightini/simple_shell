7afcc1022c9ef05ddaf0ddc800fdda2b1a0ac9db
Brightini <brightiniabasi@gmail.com>
2023-02-14 18:03:35 +0100

    Initial commit :tada: :construction:

---
+++ shell.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - receives a command for execution
+ *
+ * @ac: (int) argument count
+ * @av: (array of string) argument vector
+ * @env: (array of string) emvronment vector
+ * Return: Always 0.
+ */
+int main(int ac, char **av, char **env)
+{
+	if (ac == 1)
+		prompt(av, env);
+
+	return (0);
+}
